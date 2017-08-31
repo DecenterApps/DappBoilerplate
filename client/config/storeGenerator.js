774809d57186878cfc30132ae7d7a926966ffc07
astankovic <aleksa.stankovic@berkeley.edu>
2017-08-31 10:13:10 +0200

    Delete excess code

--- client/config/storeGenerator.js
+++ client/config/storeGenerator.js
@@ -7,18 +7,3 @@ const reduxDevToolsEnchancer = window.__REDUX_DEVTOOLS_EXTENSION__ && // eslint-
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 export default createStoreWithMiddleware(reducers, reduxDevToolsEnchancer);
-client
-webpack
-  .babelrc
-  .eslintrc.js
-  .gitignore
-package.json
-README.md
-yarn.lockclient
-webpack
-  .babelrc
-  .eslintrc.js
-  .gitignore
-package.json
-README.md
-yarn.lock
